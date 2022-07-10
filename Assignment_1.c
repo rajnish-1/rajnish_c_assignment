79563a23e405525b591ef7d80236c82bffb8ee36
rajnish-1 <109019765+rajnish-1@users.noreply.github.com>
2022-07-10 01:57:17 -0700

    first commit

---
+++ Assignment_1.c
@@ -0,0 +1,122 @@
+
+//1.Write a program to print Hello Students on the screen.
+
+/*#include<stdio.h>
+int main()
+{
+    printf("Hello Students");
+    return 0;
+}
+*/
+
+//2. Write a program to print Hello in first line and Students in the second line.
+/*
+#include<stdio.h>
+int main()
+{
+    printf("Hello\nStudents");
+    return 0;
+}
+*/
+
+//3. Write a program to print "MySirG" on the screen.
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"""MySirG""\"");
+    return 0;
+}
+*/
+
+//4. WAP to find the area of the circle. Take radius of circle from user as input and print the result in below formate.
+/*
+#include<stdio.h>
+int main()
+{
+    float R;
+    const pi = 3.14;
+    printf("Enter the radius of circle \n");
+    scanf("%f",&R);
+    float A = 2*pi*R;
+    printf("Area of circle is %f having the radius %f",A,R);
+    return 0;
+}
+*/
+
+//WAP to calculate the length of string using printf function.
+
+/*
+#include<stdio.h>
+int main()
+{
+    char str[]  = "saurbh";
+    int length;
+    length = strlen(str);
+    printf("%d",length);
+    return 0;
+}
+*/
+
+//6. WAP to print the name of user in double qoutes.
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"""Hello, Rajnish Kumar""\"");
+    return 0;
+}
+*/
+
+//7. WAP to print "%d" on the screen
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"""%%d""\"");
+    return 0;
+
+}
+*/
+
+//8. WAP to print "\n" on the screen.
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"""\\n""\"");
+    return 0;
+}
+*/
+
+//9. WAP to print "\\" on the screen
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"""\\""\\""\"");
+    return 0;
+}
+*/
+
+//10. WAP to take date as an input in below given formate and convert the data formate and diaplY THE RESULT AS GIVEN BELOW.
+/*
+#include<stdio.h>
+int main()
+{
+    char str[15];
+    printf("Enter the date DD/MM/YYYY formate \n");
+    scanf("%s",&str);
+    printf("%s",str);
+}
+*/
+
+//12. find the output of below code:
+/*#include<stdio.h>
+int main()
+{
+    int x = printf("ineuron");
+    printf("%d",x);
+    return 0;
+}*/
+//output comes --- ineuron7
